83218757b64af39c8194810b7181579242326b77
dapitdh <dapitharianto1912@gmail.com>
2025-09-08 16:00:28 +0700

    Commit No. 1 - Add Fragment Lifecyle Loggin

--- app/src/main/java/com/example/lab_week_03/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_03/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+//Commit No. 1 – add fragment lifecyle logging
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

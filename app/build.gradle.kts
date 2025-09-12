3f4bbf9db2736c447ed12991aa3013d28ca32f47
dapitdh <dapitharianto1912@gmail.com>
2025-09-12 17:36:02 +0700

    Commit No. 4 - Use Jetpack Navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
 }
